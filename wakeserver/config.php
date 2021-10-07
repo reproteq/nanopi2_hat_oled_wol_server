454d1aeebbe99d2c51eb323e1311d11052d725f4
Alex Gregori Tomás <reproteq@gmail.com>
2021-10-07 16:54:40 +0200

    Add files via upload

---
+++ wakeserver/config.php
@@ -0,0 +1,24 @@
+<?php
+	
+	$USE_HTTPS = false;
+
+    $UserName = "usernamexxxx";
+
+	$APPROVED_HASH = "passdwxxxx";
+
+	$MAX_PINGS = 15;
+
+	$SLEEP_TIME = 5;
+	
+	$COMPUTER_NAME = array("computer1","computer2");
+	
+	$COMPUTER_MAC = array("40:8d:5c:xx:xx:xx","8c:16:45:xx:xx:xx");
+
+	$COMPUTER_LOCAL_IP = array("192.168.1.xx","192.168.1.xx");
+	
+	$COMPUTER_SLEEP_CMD_PORT = xxxx;
+	
+	$COMPUTER_SLEEP_CMD = "suspend";
+
+	$BOOTSTRAP_LOCATION_PREFIX = "";
+?>
